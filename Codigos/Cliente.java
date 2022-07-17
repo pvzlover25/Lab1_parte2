3e42778a696bd56cbe8e8a60a9d96e666ed69540
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2022-07-17 19:14:01 -0400

    Create Cliente.java

---
+++ Codigos/Cliente.java
@@ -0,0 +1,10 @@
+package laboratorio2;
+import Cliente.VentanaCliente;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        VentanaCliente window=new VentanaCliente();
+    }
+    
+}
